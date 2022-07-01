f7b47be6a75305821f45ee20da1fc680ca194e5c
Igor Pavlenko <hariks@Igors-MacBook-Pro.local>
2022-07-01 18:06:48 +0300

    Initial commit

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index(): \Illuminate\Http\JsonResponse
+    {
+        return response()->json(['hello'=>'MacPaw Internship 2022!']);
+    }
+}
